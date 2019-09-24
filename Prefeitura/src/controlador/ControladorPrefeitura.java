bea9ab4543bc03b667425ca111c207fe3a185a33
Administrador <Administrador@10.10.4.2>
2019-09-24 16:49:43 -0300

    teste

---
+++ Prefeitura/src/controlador/ControladorPrefeitura.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import dao.PrefeituraDao;
+import javax.swing.JOptionPane;
+import modelo.Prefeitura;
+import tela.manutencao.ManutencaoPrefeitura;
+
+import java.util.List;
+
+import java.util.Vector;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+/**
+ *
+ * @author Administrador
+ */
+public class ControladorPrefeitura {
+
+    public static void inserir(ManutencaoPrefeitura man){
+        Prefeitura objeto = new Prefeitura();
+        objeto.setNome(man.jtfNome.getText());
+        objeto.setEndereco(man.jtfEndereco.getText());
+        objeto.setNr_funcionario(Integer.parseInt(man.jtfNr_funcionarios.getText()));
+        
+        boolean resultado = PrefeituraDao.inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+}
+
+    public static void alterar(ManutencaoPrefeitura man){
+        Prefeitura objeto = new Prefeitura();
+        //definir todos os atributos
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText()));
+        objeto.setNome(man.jtfNome.getText());
+        objeto.setEndereco(man.jtfEndereco.getText());
+        objeto.setNr_funcionario(Integer.parseInt(man.jtfNr_funcionarios.getText()));
+        
+        boolean resultado = PrefeituraDao.alterar(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+
+     public static void excluir(ManutencaoPrefeitura man){
+        Prefeitura objeto = new Prefeitura();
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText())); //só precisa definir a chave primeira
+        
+        boolean resultado = PrefeituraDao.excluir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+    
+     public static void atualizarTabela(JTable tabela) {
+        DefaultTableModel modelo = new DefaultTableModel();
+        //definindo o cabeçalho da tabela
+        modelo.addColumn("Codigo");
+        modelo.addColumn("Nome");
+        modelo.addColumn("Endereço");
+        modelo.addColumn("Funcionários");
+        List<Prefeitura> resultados = PrefeituraDao.consultar();
+        for (Prefeitura objeto : resultados) {
+            Vector linha = new Vector();
+            
+            //definindo o conteúdo da tabela
+            linha.add(objeto.getCodigo());
+            linha.add(objeto.getNome());
+            linha.add(objeto.getEndereco());
+            linha.add(objeto.getNr_funcionario());
+            modelo.addRow(linha); //adicionando a linha na tabela
+        }
+        tabela.setModel(modelo);
+    }
+     
+     public static void atualizaCampos(ManutencaoPrefeitura man, int pk){ 
+        Prefeitura objeto = PrefeituraDao.consultar(pk);
+        //Definindo os valores do campo na tela (um para cada atributo/campo)
+        man.jtfCodigo.setText(objeto.getCodigo().toString());
+        man.jtfNome.setText(objeto.getNome());
+        man.jtfEndereco.setText(objeto.getEndereco());
+        man.jtfNr_funcionarios.setText(objeto.getNr_funcionario().toString());
+        
+        man.jtfCodigo.setEnabled(false); //desabilitando o campo código
+        man.btnAdicionar.setEnabled(false); //desabilitando o botão adicionar
+    }
+}
